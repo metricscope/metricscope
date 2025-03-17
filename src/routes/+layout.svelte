99423462f852f28cd0730fa3e813734602edd9dc
Tom Bull <tom@tombull.com>
2025-03-17 00:08:57 +0000

    Initial commit - generated by SvelteKit and slightly modified

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
+	let { children } = $props();
+	export const prerender = true;
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
